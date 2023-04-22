a74e6acab8aec422e00bd46fd3640b7b68a1e525
mohammed qasim <87497268+MohammedTsmu@users.noreply.github.com>
2023-04-22 19:27:41 +0300

    Delete Kid-food-picker directory

--- Kid-food-picker/js/footer.js
+++
@@ -1,3 +0,0 @@
-const year = new Date().getFullYear();
-const footerText = document.getElementById("footerText");
-footerText.innerHTML = ` ${year}&copy; Made with <i class="fa-solid fa-heart" style="color: #ff0000;"></i> by Mohammed-qasim`;
